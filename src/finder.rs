e2386376453c59ff5e913e904f67503198915724
Francesco Banconi <francesco.banconi@canonical.com>
2020-11-27 15:02:17 +0100

    Improve finder module test coverage

--- src/finder.rs
+++ src/finder.rs
@@ -78,7 +78,7 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn run_get_account_ok() {
+    async fn run_from_id_ok_get_account_ok() {
         let q = "0012500001Lhk3hAAB";
         let config = Config::empty();
         // TODO(frankban): is this better than a RefCell in the mock client?
@@ -96,7 +96,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn run_run_get_account_not_found() {
+    async fn run_from_id_ok_get_account_not_found() {
         let q = "0012500001Lhk3hAAB";
         let config = Config::empty();
         let client = TestClient::new(|args| match args {
@@ -114,7 +114,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn run_get_account_error() {
+    async fn run_from_id_ok_get_account_error() {
         let q = "0012500001Lhk3hAAB";
         let config = Config::empty();
         let client = TestClient::new(|args| match args {
@@ -131,7 +131,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn run_get_by_field_error() {
+    async fn run_from_id_error() {
         let q = "02i2500000HTaW9AAL";
         let config = Config::empty();
         let client = TestClient::new(|args| match args {
@@ -145,11 +145,184 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn run_get_by_id_field_error() {
+    async fn run_from_extra_ok_get_account_ok() {
         let q = "02i2500000HTaW9AAL";
-        let config = Config::empty();
+        let config = Config {
+            additional_fields: vec![],
+            search_fields: vec![
+                "Account.SomeField".parse::<sf::EntityField>().unwrap(),
+                "Opportunity.AnotherField"
+                    .parse::<sf::EntityField>()
+                    .unwrap(),
+            ],
+        };
         let client = TestClient::new(|args| match args {
             MockArgs::GetAccountIDByField("Asset.Id", "02i2500000HTaW9AAL") => {
+                MockResult::Err(sf::Error::NotFound)
+            }
+            MockArgs::GetAccountIDByField("Account.SomeField", "02i2500000HTaW9AAL") => {
+                MockResult::Err(sf::Error::NotFound)
+            }
+            MockArgs::GetAccountIDByField("Opportunity.AnotherField", "02i2500000HTaW9AAL") => {
+                MockResult::ID(String::from("0012500001Lhk3hAAB"))
+            }
+            MockArgs::GetAccount("0012500001Lhk3hAAB") => {
+                MockResult::Account(sf::Account::new_for_tests())
+            }
+            _ => panic!("unhandled request/response: {:?}", args),
+        });
+        let acc = run(client, q, config).await.unwrap();
+        assert_eq!(acc.id, "id-for-tests");
+    }
+
+    #[tokio::test]
+    async fn run_from_extra_ok_get_account_not_found() {
+        let q = "some-query";
+        let config = Config {
+            additional_fields: vec![],
+            search_fields: vec!["Account.SomeField".parse::<sf::EntityField>().unwrap()],
+        };
+        let client = TestClient::new(|args| match args {
+            MockArgs::GetAccountIDByField("Account.SomeField", "some-query") => {
+                MockResult::ID(String::from("0012500001Lhk3hAAB"))
+            }
+            MockArgs::GetAccount("0012500001Lhk3hAAB") => MockResult::Err(sf::Error::NotFound),
+            _ => panic!("unhandled request/response: {:?}", args),
+        });
+        let err = run(client, q, config).await.unwrap_err();
+        assert_eq!(err.message, "nothing found for query \"some-query\"");
+    }
+
+    #[tokio::test]
+    async fn run_from_extra_ok_get_account_error() {
+        let q = "some-query";
+        let config = Config {
+            additional_fields: vec![],
+            search_fields: vec!["Asset.OpportunityId__c".parse::<sf::EntityField>().unwrap()],
+        };
+        let client = TestClient::new(|args| match args {
+            MockArgs::GetAccountIDByField("Asset.OpportunityId__c", "some-query") => {
+                MockResult::ID(String::from("0012500001Lhk3hAAA"))
+            }
+            MockArgs::GetAccount("0012500001Lhk3hAAA") => {
+                MockResult::Err(sf::Error::Message(String::from("bad wolf")))
+            }
+            _ => panic!("unhandled request/response: {:?}", args),
+        });
+        let err = run(client, q, config).await.unwrap_err();
+        assert_eq!(err.message, "bad wolf");
+    }
+
+    #[tokio::test]
+    async fn run_from_extra_not_found() {
+        let q = "some-query";
+        let config = Config {
+            additional_fields: vec![],
+            search_fields: vec![
+                "Account.SomeField".parse::<sf::EntityField>().unwrap(),
+                "Opportunity.AnotherField"
+                    .parse::<sf::EntityField>()
+                    .unwrap(),
+            ],
+        };
+        let client = TestClient::new(|args| match args {
+            MockArgs::GetAccountIDByField("Account.SomeField", "some-query") => {
+                MockResult::Err(sf::Error::NotFound)
+            }
+            MockArgs::GetAccountIDByField("Opportunity.AnotherField", "some-query") => {
+                MockResult::Err(sf::Error::NotFound)
+            }
+            _ => panic!("unhandled request/response: {:?}", args),
+        });
+        let err = run(client, q, config).await.unwrap_err();
+        assert_eq!(err.message, "nothing found for query \"some-query\"");
+    }
+
+    #[tokio::test]
+    async fn run_from_extra_not_found_no_fields() {
+        let q = "some-query";
+        let config = Config::empty();
+        let client = TestClient::new(|args| match args {
+            _ => panic!("unhandled request/response: {:?}", args),
+        });
+        let err = run(client, q, config).await.unwrap_err();
+        assert_eq!(err.message, "nothing found for query \"some-query\"");
+    }
+
+    #[tokio::test]
+    async fn run_from_extra_error() {
+        let q = "some-query";
+        let config = Config {
+            additional_fields: vec![],
+            search_fields: vec![
+                "Account.SomeField".parse::<sf::EntityField>().unwrap(),
+                "Opportunity.AnotherField"
+                    .parse::<sf::EntityField>()
+                    .unwrap(),
+            ],
+        };
+        let client = TestClient::new(|args| match args {
+            MockArgs::GetAccountIDByField("Account.SomeField", "some-query") => {
+                MockResult::Err(sf::Error::Message(String::from("bad wolf")))
+            }
+            _ => panic!("unhandled request/response: {:?}", args),
+        });
+        let err = run(client, q, config).await.unwrap_err();
+        assert_eq!(err.message, "bad wolf");
+    }
+
+    #[tokio::test]
+    async fn run_from_email_ok_get_account_ok() {
+        let q = "who@example.com";
+        let config = Config {
+            additional_fields: vec![],
+            search_fields: vec!["Account.SomeField".parse::<sf::EntityField>().unwrap()],
+        };
+        let client = TestClient::new(|args| match args {
+            MockArgs::GetAccountIDByField("Contact.email", "who@example.com") => {
+                MockResult::ID(String::from("0012500001Lhk3hAAB"))
+            }
+            MockArgs::GetAccount("0012500001Lhk3hAAB") => {
+                MockResult::Account(sf::Account::new_for_tests())
+            }
+            _ => panic!("unhandled request/response: {:?}", args),
+        });
+        let acc = run(client, q, config).await.unwrap();
+        assert_eq!(acc.id, "id-for-tests");
+    }
+
+    #[tokio::test]
+    async fn run_from_email_not_found_get_account_ok() {
+        let q = "who@example.com";
+        let config = Config {
+            additional_fields: vec![],
+            search_fields: vec!["Account.SomeField".parse::<sf::EntityField>().unwrap()],
+        };
+        let client = TestClient::new(|args| match args {
+            MockArgs::GetAccountIDByField("Contact.email", "who@example.com") => {
+                MockResult::Err(sf::Error::NotFound)
+            }
+            MockArgs::GetAccountIDByField("Account.SomeField", "who@example.com") => {
+                MockResult::ID(String::from("0012500001Lhk3hAAB"))
+            }
+            MockArgs::GetAccount("0012500001Lhk3hAAB") => {
+                MockResult::Account(sf::Account::new_for_tests())
+            }
+            _ => panic!("unhandled request/response: {:?}", args),
+        });
+        let acc = run(client, q, config).await.unwrap();
+        assert_eq!(acc.id, "id-for-tests");
+    }
+
+    #[tokio::test]
+    async fn run_from_email_error() {
+        let q = "who@example.com";
+        let config = Config {
+            additional_fields: vec![],
+            search_fields: vec!["Account.SomeField".parse::<sf::EntityField>().unwrap()],
+        };
+        let client = TestClient::new(|args| match args {
+            MockArgs::GetAccountIDByField("Contact.email", "who@example.com") => {
                 MockResult::Err(sf::Error::Message(String::from("bad wolf")))
             }
             _ => panic!("unhandled request/response: {:?}", args),
